119543f4eca5aded6f06fb2b0c21e6d3a07c6b37
Tianhao Wang <tianhao.wang@slu.edu>
2023-04-09 10:42:27 -0500

    Backend

--- Backend/Backend/test.py
+++ Backend/Backend/test.py
@@ -1,24 +1,31 @@
-import requests
+import unittest
+import json
+from API import app
 
-# Set the URL for the find_sections endpoint
-url = "http://localhost:5000/find-sections"
+class TestFindSections(unittest.TestCase):
 
-# Set the file path and search terms for the request
-file_path = "/Users/samsam/Desktop/EXAMPLE.txt"
-search_terms = ["CARTESIAN COORDINATES (A.U.)", "INTERNAL COORDINATES (ANGSTROEM)", "BASIS SET INFORMATION"]
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
 
-# Create the request JSON
-request_data = {
-    "file_path": file_path,
-    "search_terms": search_terms
-}
+    def test_missing_fields(self):
+        # Test missing fields in the request
+        data = {}
+        response = self.app.post('/find-sections', data=json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
 
-# Send the request to the endpoint
-response = requests.post(url, json=request_data)
+    def test_valid_request(self):
+        # Test a valid request
+        data = {
+            'file_path': '/Users/samsam/Desktop/EXAMPLE.txt',
+            'search_terms': ["CARTESIAN COORDINATES (A.U.)"],
+            'sections': [1],
+            'specifyLines': ['LAST 5'],
+            'use_total_lines': False,
+            'lines': 10
+        }
+        response = self.app.post('/find-sections', data=json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
-# Check the response status code
-if response.status_code == 200:
-    print("Request successful.")
-else:
-    print(f"Request failed with status code {response.status_code}.")
-
+if __name__ == '__main__':
+    unittest.main()
