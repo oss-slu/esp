5053736d22dcf51cf45e4a3692021003d2f5c5f7
mkalal6 <137332780+mkalal6@users.noreply.github.com>
2024-02-26 14:19:17 -0600

    changing frontend components

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,7 +1,6 @@
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-def create_app(config_name):
+def create_app():
     app = Flask(__name__)
     @app.route('/api/data', methods=['GET'])
     def get_data():
@@ -13,10 +12,9 @@ def create_app(config_name):
         message = message_data.get('message', '')
         print(f"Received message: {message}")
         return jsonify({"status": "success", "message": "Message received successfully!"})
-    config_module = f"application.config.{config_name.capitalize()}Config"
-    app.config.from_object(config_module)
-    app.register_blueprint(scantron.blueprint)
-    CORS(app)  
+    CORS(app)
     return app
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
