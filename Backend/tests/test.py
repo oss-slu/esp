0a0915c9d5ff6171a709efb50ae0470babcadbae
Hayden <hayden.karl@slu.edu>
2024-04-22 14:35:34 -0500

    updated imports and made file paths universal to fix test cases

--- Backend/tests/test.py
+++ Backend/tests/test.py
@@ -1,11 +1,24 @@
+import os
 import sys
-sys.path.insert(0, '/Users/samsam/orca_converter/Backend/src')
-
 import unittest
 import json
-from API import app
+
+#Get absolute path to the directory containing the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+#Get the path for the project directory
+project_dir = os.path.join(current_dir, '..', 'src')
+
+#Add file_path directory
+file_path = os.path.abspath(os.path.join(current_dir, '..', '..', 'inputs', 'working', 'Styrene-ad1.txt'))
+
+#Add this path to sys.path to allow script to work universally
+sys.path.insert(0, project_dir)
+#sys.path.insert(0, '/Users/seala/csci4961/esp/Backend/src')
 
 
+from app import app_instance as app
+print(sys.path)
 
 
 class TestFindSections(unittest.TestCase):
@@ -23,10 +36,10 @@ class TestFindSections(unittest.TestCase):
     def test_valid_request(self):
         # Test a valid request
         data = {
-            'file_path': '/Users/samsam/Desktop/EXAMPLE.txt',
+            'file_path': file_path,
             'search_terms': ["CARTESIAN COORDINATES (A.U.)"],
             'sections': [1],
-            'specifyLines': ['LAST 5'],
+            'specify_lines': 'LAST 5',
             'use_total_lines': False,
             'lines': 10
         }
