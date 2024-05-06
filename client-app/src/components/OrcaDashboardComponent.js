daf00df8b469ae695ae2d7b4030dc3a0642c14fd
mkalal6 <137332780+mkalal6@users.noreply.github.com>
2024-05-06 16:26:57 -0500

    temp page created for issue #51 changes

--- client-app/src/components/OrcaDashboardComponent.js
+++ client-app/src/components/OrcaDashboardComponent.js
@@ -1,22 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { saveAs } from 'file-saver';
-import '../styles/DashboardComponent.css';
+import '../styles/DashboardComponent.css'
 
 const OrcaDashboardComponent = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [fileName, setFileName] = useState('');
-  const [searchTerms, setSearchTerms] = useState([]);
-  const [specifyLines, setSpecifyLines] = useState([]);
-  const [sections, setSections] = useState([]);
-  const [useTotalLines, setUseTotalLines] = useState([]);
-  const [totalLines, setTotalLines] = useState([]);
-  const [showCard, setShowCard] = useState(false);
+  const [searchTerms, setSearchTerms] = useState('');
+  const [specifyLines, setSpecifyLines] = useState('');
+  const [sections, setSections] = useState('');
+  const [useTotalLines, setUseTotalLines] = useState('');
+  const [totalLines, setTotalLines] = useState('');
 
   const onFileSelected = (event) => {
-    const selectedFile = event.target.files[0];
-    setSelectedFile(selectedFile);
-    setFileName(selectedFile.name.split('/').pop()); 
+    setSelectedFile(event.target.files[0]);
   };
 
   const onUpload = () => {
@@ -29,15 +26,14 @@ const OrcaDashboardComponent = () => {
     formData.append('file', selectedFile);
 
     axios
-    .post('http://localhost:5000/upload', formData)
-    .then((response) => {
-      console.log('File uploaded successfully:', response);
-      const uploadedFileName = response.data.filename.split('/').pop();
-      setFileName(uploadedFileName); 
-    })
-    .catch((error) => {
-      console.error('Error uploading file:', error);
-    });
+      .post('http://localhost:5000/upload', formData)
+      .then((response) => {
+        console.log('File uploaded successfully:', response);
+        setFileName(response.data.filename);
+      })
+      .catch((error) => {
+        console.error('Error uploading file:', error);
+      });
   };
 
   const onSubmit = () => {
@@ -48,9 +44,9 @@ const OrcaDashboardComponent = () => {
 
     const data = {
       file_path: fileName.toString(),
-      search_terms: searchTerms,
-      sections: sections,
-      specify_lines: specifyLines.join(','),
+      search_terms: searchTerms.split(','),
+      sections: sections.split(','),
+      specify_lines: specifyLines.toString(),
     };
 
     if (useTotalLines) {
@@ -78,32 +74,6 @@ const OrcaDashboardComponent = () => {
     saveAs(blob, 'output.docx');
   };
 
-  const handleKeyPress = (e, setterFunc) => {
-    if (e.key === 'Enter' || e.key === ',') {
-      e.preventDefault();
-      const value = e.target.value.trim();
-      if (value) {
-        const values = value.split(','); 
-        setterFunc(prevValue => [...prevValue, ...values.map(val => val.trim().toUpperCase())]);
-        e.target.value = '';
-      }
-    }
-  };
-  
-  
-
-  const removeTag = (index, setterFunc) => {
-    setterFunc((prevTerms) => {
-      const updatedTerms = [...prevTerms];
-      updatedTerms.splice(index, 1);
-      return updatedTerms;
-    });
-  };
-
-  const handleSameCriteriaChange = (e) => {
-    setShowCard(e.target.checked); 
-  };
-
   return (
     <div className="container py-5 d-flex justify-content-center">
       <div className="text-center">
@@ -124,124 +94,65 @@ const OrcaDashboardComponent = () => {
         </div>
 
         <div className="mb-3 text-start">
-        <span>Enter the terms you wish to search for (txt only):</span>
-        <div>
+          <span>Enter the terms you wish to search for (txt only):</span>
           <input
             type="text"
             className="form-control"
             placeholder="E.g., CARTESIAN COORDINATES"
-            onKeyPress={(e) => handleKeyPress(e, setSearchTerms)}
+            value={searchTerms}
+            onChange={(e) => setSearchTerms(e.target.value.toUpperCase())}
           />
-          {searchTerms.map((term, index) => (
-            <span
-              key={index}
-              className="badge bg-secondary me-2 mb-2"
-              onClick={() => removeTag(index, setSearchTerms)}
-            >
-              {term}
-              <button
-                type="button"
-                className="btn-close ms-1"
-                aria-label="Remove"
-              ></button>
-            </span>
-          ))}
         </div>
-        {searchTerms.length > 1 && ( 
-          <div className="form-check mt-2">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="sameCriteriaCheckbox"
-              checked={showCard}
-              onChange={handleSameCriteriaChange}
-            />
-            <label className="form-check-label" htmlFor="sameCriteriaCheckbox">
-              Is the search criteria same for all search terms
-            </label>
-          </div>
-        )}
-      </div>
 
-      
-      <div className="mb-3 text-start">
-        <span>Enter how you want the lines specified:</span>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="E.g., WHOLE, FIRST X, LAST X"
-          value={specifyLines.join(' ')} 
-          onChange={(e) => setSpecifyLines(e.target.value.split(/,\s*|\s+/).map(val => val.trim().toUpperCase()))} 
-        />
-      </div>
-
-      <div className="mb-3 text-start">
-        <span>Number of sections?</span>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Input as number..."
-          value={sections.join(', ')} 
-          onChange={(e) => setSections(e.target.value.split(',').map(val => val.trim()))} 
-        />
-      </div>
-
-      <div className="mb-3 text-start">
-        <span>Use total lines?</span>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="TRUE/FALSE"
-          value={useTotalLines.join(', ')} 
-          onChange={(e) => setUseTotalLines(e.target.value.split(',').map(val => val.trim().toUpperCase()))} 
-        />
-      </div>
-
-      <div className="mb-3 text-start">
-        <span>Total number of lines for output doc?</span>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Input as number..."
-          value={totalLines.join(', ')} 
-          onChange={(e) => setTotalLines(e.target.value.split(',').map(val => val.trim()))}
-        />
-      </div>
-
-      <button className="btn btn-primary" onClick={onSubmit}>
-          Submit Search Query 
-      </button>
-
-      {showCard && (
-        <div className="card mt-3">
-          <div className="card-body">
-            <h5 className="card-title">Search Query</h5>
-            <p className="card-text">Search Terms: {searchTerms.join(', ')}</p>
-            <p className="card-text">Lines Specified: {specifyLines.join(', ')}</p>
-            <p className="card-text">Sections: {sections.join(', ')}</p>
-            <div className="d-flex justify-content-end">
-              <button className="btn btn-primary me-2">Edit</button>
-              <button className="btn btn-danger">Delete</button>
-            </div>
-          </div>
+        <div className="mb-3 text-start">
+          <span>Enter how you want the lines specified:</span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="E.g., WHOLE, FIRST X, LAST X"
+            value={specifyLines}
+            onChange={(e) => setSpecifyLines(e.target.value.toUpperCase())}
+          />
         </div>
-      )}
 
-      <button 
-          className="btn btn-primary" 
-          onClick={onSubmit} 
-          disabled={
-            !searchTerms.length ||
-            !specifyLines.length ||
-            !sections.length ||
-            !useTotalLines.length ||
-            !totalLines.length ||
-            !selectedFile
-          }
-        >
+        <div className="mb-3 text-start">
+          <span>Number of sections?</span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Input as number..."
+            value={sections}
+            onChange={(e) => setSections(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-3 text-start">
+          <span>Use total lines?</span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="TRUE/FALSE"
+            value={useTotalLines}
+            onChange={(e) => setUseTotalLines(e.target.value.toUpperCase())}
+          />
+        </div>
+
+        <div className="mb-3 text-start">
+          <span>Total number of lines for output doc?</span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Input as number..."
+            value={totalLines}
+            onChange={(e) => {
+              const inputValue = e.target.value;
+              setTotalLines(inputValue === '' ? '' : parseInt(inputValue));
+            }}
+          />
+        </div>
+        <button className="btn btn-primary" onClick={onSubmit}>
           Download Output
         </button>
-        
       </div>
     </div>
   );
