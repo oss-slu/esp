d6f217400c09adb884b0dbf110155935966dc2f8
Aonyx <slyskawaschool@Sams-MacBook-Pro-2.local>
2024-09-30 11:54:33 -0500

    fixed issue where output contained non-relevant information

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from docx import Document
-from pathlib import Path
 import logging
 import os
-import json
 
 def create_app():
     app = Flask(__name__)
@@ -14,64 +12,72 @@ def create_app():
     os.makedirs(uploads_dir, exist_ok=True)
 
     def extract_sections(file_path, search_terms, sections, specify_lines, use_total_lines, total_lines):
-        with open(file_path, 'r') as f:
-            Lines = f.readlines()
+        '''
+        Extracts the data from orca log file based on search terms and sections.
+        '''
+        with open(file_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
 
         document_content = ""
 
         for term in search_terms:
-            lineNo = 0
-            termLineNo = []
-            termsNum = 0
-            for line in Lines:
+            line_num = 0
+            term_line_num = []
+            terms_num = 0
+            for line in lines:
                 if term in line:
-                    termLineNo.append(lineNo)
-                    termsNum += 1
-                lineNo += 1
+                    term_line_num.append(line_num)
+                    terms_num += 1
+                line_num += 1
 
         for i in sections:
             section_lines = specify_lines[i-1].split()
-            start_line = termLineNo[i-1]
+            start_line = term_line_num[i-1]
             line_empty = 0
+
+            def is_content_line(line):
+                return not (line.strip() == "" or line.startswith("-----") or line.startswith("Title:"))
 
             if section_lines[0].upper() == 'WHOLE' and not use_total_lines:
                 while line_empty == 0:
-                    if Lines[start_line] != "\n":
-                        document_content += Lines[start_line]
+                    if lines[start_line] != "\n" and is_content_line(lines[start_line]):
+                        document_content += lines[start_line]
                         start_line += 1
                     else:
                         line_empty = 1
 
-            if section_lines[0].upper() == 'WHOLE' and use_total_lines:
-                for _ in range(total_lines - start_line + termLineNo[i-1]):
-                    document_content += Lines[start_line]
-                    start_line += 1
-                    line_empty = 1
-                else:
+            elif section_lines[0].upper() == 'WHOLE' and use_total_lines:
+                for _ in range(total_lines - start_line + term_line_num[i-1]):
+                    if is_content_line(lines[start_line]):
+                        document_content += lines[start_line]
                     start_line += 1
                     line_empty = 1
 
             elif section_lines[0].upper() == 'FIRST':
                 line_count = 0
                 while line_count < int(section_lines[1]):
-                    document_content += Lines[start_line]
+                    if is_content_line(lines[start_line]):
+                        document_content += lines[start_line]
+                        line_count += 1
                     start_line += 1
-                    line_count += 1
 
             elif section_lines[0].upper() == 'LAST':
                 line_count = 0
-                document_content += Lines[start_line]
-                document_content += Lines[start_line + 1]
+                document_content += lines[start_line]
+                document_content += lines[start_line + 1]
                 while line_count < int(section_lines[1]) - 1:
-                    document_content += Lines[start_line+10]
+                    if is_content_line(lines[start_line + 10]):
+                        document_content += lines[start_line + 10]
                     start_line += 1
                     line_count += 1
 
             elif section_lines[0].upper() == 'SPECIFIC':
                 specific_lines = [int(l) for l in section_lines[1].split(",")]
-                document_content += Lines[start_line]
+                if is_content_line(lines[start_line]):
+                    document_content += lines[start_line]
                 for l in specific_lines:
-                    document_content += Lines[start_line + l + 1]
+                    if is_content_line(lines[start_line + l + 1]):
+                        document_content += lines[start_line + l + 1]
 
         return document_content
 
@@ -101,17 +107,17 @@ def create_app():
         if 'file' not in request.files:
             logging.error('No file part in request')
             return {'message': 'No file part'}, 400
-        
+
         file = request.files['file']
-        
+
         if file.filename == '':
             logging.error('No selected file')
             return {'message': 'No selected file'}, 400
-        
+
         if file and file.mimetype == 'text/plain':
             filename = os.path.join(uploads_dir, file.filename)
             file.save(filename)
-            return {'message': 'Success', 'filename':filename}, 200  
+            return {'message': 'Success', 'filename': filename}, 200
         else:
             logging.error('Invalid file type')
             return {'message': 'Invalid file type'}, 400
